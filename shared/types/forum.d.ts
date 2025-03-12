73f1f10bda2375f6c4d17d97a10e8d232d03161c
maxedhunter <maxwelljhe@gmail.com>
2025-03-12 10:51:26 -0400

    forum type added to shared

---
+++ shared/types/forum.d.ts
@@ -0,0 +1,31 @@
+import { Request } from 'express';
+import { ObjectId } from 'mongodb';
+import { User, DatabaseUser } from './user';
+import { Question } from './question';
+
+/**
+ * Represents a forum in the database.
+ * 
+ */
+export interface Forum {
+  name: string;
+  description: string;
+  flairs: string[];
+  createdBy: string;
+  createDateTime: Date;
+  moderators: User[];
+  members: User[];
+  questions: Question[];
+  type: 'public' | 'private';
+}
+
+/**
+ * Represents a forum in the database.
+ * 
+ */
+export interface DatabaseForum extends Forum {
+  _id: ObjectId;
+  moderators: ObjectId[];
+  members: ObjectId[];
+  questions: ObjectId[];
+}
