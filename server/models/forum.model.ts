c473cdc3074a9f0c146d21b9f8913922d09bf1a2
maxedhunter <maxwelljhe@gmail.com>
2025-03-12 10:56:32 -0400

    added forum to models

--- server/models/forum.model.ts
+++ server/models/forum.model.ts
@@ -2,6 +2,15 @@ import mongoose, { Model } from 'mongoose';
 import forumSchema from './schema/forum.schema';
 import { DatabaseForum } from '../types/types';
 
+/**
+ * Mongoose model for the Forum collection.
+ *
+ * This model is created using the `Forum` interface and the `forumSchema`, representing the
+ * `Forum` collection in the MongoDB database, and provides an interface for interacting with
+ * the stored forums.
+ *
+ * @type {Model<DatabaseForum>}
+ */
 const ForumModel: Model<DatabaseForum> = mongoose.model<DatabaseForum>('Forum', forumSchema);
 
 export default ForumModel;
---
+++ server/models/schema/forum.schema.ts
@@ -0,0 +1,47 @@
+import { Schema } from 'mongoose';
+
+/**
+ * Mongoose schema for the Forum collection.
+ *
+ * This schema defines the structure for storing forums in the database.
+ * Each forum includes the following fields:
+ * - `name`: The name of the forum.
+ * - `description`: A brief description of the forum.
+ * - `flairs`: An array of strings representing available flairs for posts in the forum.
+ * - `createdBy`: The user who created the forum.
+ * - `createDateTime`: The date and time when the forum was created.
+ * - `moderators`: An array of references to Users who moderate the forum.
+ * - `members`: An array of references to Users who are members of the forum.
+ * - `questions`: An array of references to Questions posted in the forum.
+ * - `type`: The type of forum, either 'public' or 'private'.
+ */
+
+const forumSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    flairs: [{ type: String }],
+    createdBy: {
+      type: String,
+    },
+    createDateTime: {
+      type: Date,
+    },
+    moderators: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
+    type: {
+      type: String,
+      enum: ['public', 'private'],
+    },
+  },
+  { collection: 'Forum' },
+);
+
+export default forumSchema;
