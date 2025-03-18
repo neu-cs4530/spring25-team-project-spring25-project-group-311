57ff6067f452cf7907bf0b0eed6880abcb102ed8
maxedhunter <maxwelljhe@gmail.com>
2025-03-18 14:18:38 -0400

    adding forum fields for banned and waiting user

--- server/tests/mockData.models.ts
+++ server/tests/mockData.models.ts
@@ -209,6 +209,8 @@ export const forum: Forum = {
   createdBy: 'user1',
   moderators: ['user1'],
   members: ['user1'],
+  awaitingMembers: [],
+  bannedMembers: [],
   questions: [],
   type: 'private',
 };
