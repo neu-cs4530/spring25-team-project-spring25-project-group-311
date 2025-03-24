3611da838185a1cf8e2dcd1ffe8240ae016c0a38
maxedhunter <maxwelljhe@gmail.com>
2025-03-24 12:05:24 -0400

    working forum list!

--- client/src/components/main/forumPage/forumCard/index.tsx
+++ client/src/components/main/forumPage/forumCard/index.tsx
@@ -1,39 +1,67 @@
 import React from 'react';
+import { ObjectId } from 'mongodb';
+import { useNavigate } from 'react-router-dom';
 import './index.css';
+import { getMetaData } from '../../../../tool';
 import { DatabaseForum } from '../../../../types/types';
 
 /**
- * Interface representing the props for the ForumCard component.
+ * Interface representing the props for the Forum component.
  *
- * forum - The forum object containing details about the forum.
- * handleForumClick - The function to handle clicking on the forum card.
+ * forum - the forum object containing details about the forum.
  */
-interface ForumCardProps {
+interface ForumProps {
   forum: DatabaseForum;
-  handleForumClick: (forumName: string) => void;
 }
 
-/**
- * ForumCard component renders the details of a forum including its name, description, and flairs.
- */
-const ForumCard = (props: ForumCardProps) => {
-  const { forum, handleForumClick } = props;
+const ForumView = ({ forum }: ForumProps) => {
+  const navigate = useNavigate();
+
+  /**
+   * Function to navigate to the home page with the specified tag as a search parameter.
+   *
+   * @param forumName - The name of the tag to be added to the search parameters.
+   */
+  const handleForum = (forumID: ObjectId) => {
+    navigate(`/forum/${forumID}`);
+  };
 
   return (
-    <div className='forum_card right_padding' onClick={() => handleForumClick(forum.name)}>
-      <div className='forum_content'>
-        <div className='forum_name'>{forum.name}</div>
-        <div className='forum_description'>{forum.description}</div>
+    <div
+      className='forum right_padding'
+      onClick={() => {
+        if (forum._id) {
+          handleForum(forum._id);
+        }
+      }}>
+      <div className='forumStats'>
+        <div>{forum.members.length || 0} members</div>
+        <div>{forum.questions.length || 0} questions</div>
+      </div>
+      <div className='forum_mid'>
+        <div className='forumTitle'>{forum.name}</div>
+        <div className='forumDescription'>{forum.description}</div>
         <div className='forum_flairs'>
           {forum.flairs.map((flair, index) => (
-            <span key={index} className='forum_flair'>
+            <button
+              key={index}
+              className='forum_flair_button'
+              onClick={e => {
+                e.stopPropagation();
+              }}>
               {flair}
-            </span>
+            </button>
           ))}
         </div>
+      </div>
+      <div className='lastActivity'>
+        <div className='forum_creator'>{forum.createdBy}</div>
+        <div>&nbsp;</div>
+        <div className='forum_meta'>created {getMetaData(new Date(forum.createDateTime))}</div>
+        <div className='forum_type'>{forum.type}</div>
       </div>
     </div>
   );
 };
 
-export default ForumCard;
+export default ForumView;
