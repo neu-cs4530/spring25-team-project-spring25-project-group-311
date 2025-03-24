5407710b484f068f5ad3d3e3dc96558c587811e3
maxedhunter <maxwelljhe@gmail.com>
2025-03-24 12:05:24 -0400

    working forum list!

--- client/src/components/main/forumPage/index.tsx
+++ client/src/components/main/forumPage/index.tsx
@@ -1,41 +1,31 @@
 import React from 'react';
 import './index.css';
-import { useNavigate } from 'react-router-dom';
 import CreateForumButton from '../createForumButton';
 import useForumPage from '../../../hooks/useForumPage';
-import ForumCard from './forumCard';
+import ForumView from './forumCard';
 
 /**
- * Represents the ForumPage component which displays forums available to join.
+ * ForumPage component renders a page displaying a list of forums.
+ * It includes a header with the forum count and a button to create a new forum.
  */
 const ForumPage = () => {
-  const navigate = useNavigate();
   const { forumList } = useForumPage();
 
-  /**
-   * Navigates to the forum details page when a forum is clicked
-   */
-  const handleForumClick = (forumName: string) => {
-    navigate(`/forum/${forumName}`);
-  };
-
   return (
-    <>
+    <div className='forum-container'>
       <div className='space_between right_padding'>
         <div className='bold_title'>Forums ({forumList?.length || 0})</div>
         <CreateForumButton />
       </div>
-
-      <div className='forums_list'>
-        {forumList && forumList.length > 0 ? (
-          forumList.map(forum => (
-            <ForumCard key={forum.name} forum={forum} handleForumClick={handleForumClick} />
-          ))
-        ) : (
-          <div className='bold_title right_padding'>No Forums Found</div>
-        )}
+      <div id='forums_list' className='forums_list'>
+        {forumList.map(forum => (
+          <ForumView forum={forum} key={forum.name} />
+        ))}
       </div>
-    </>
+      {(!forumList.length || forumList.length === 0) && (
+        <div className='bold_title right_padding'>No Forums Found</div>
+      )}
+    </div>
   );
 };
 
