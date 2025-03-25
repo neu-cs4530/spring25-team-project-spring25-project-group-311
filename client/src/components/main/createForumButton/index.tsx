1e1f0916c456dddc449f0e68307838b819e97f5d
Alexandra Hu <108170281+allexandrahu@users.noreply.github.com>
2025-03-25 13:09:26 -0400

    clean up unnecessary comments

--- client/src/components/main/createForumButton/index.tsx
+++ client/src/components/main/createForumButton/index.tsx
@@ -14,16 +14,6 @@ const CreateForumButton = () => {
     navigate('/new/forum');
   };
 
-  // const handleSortQuestions = () => {
-  //   const sortedQuestions = questions.sort((a, b) => {
-  //     const dateA = new Date(a.createdAt);
-  //     const dateB = new Date(b.createdAt);
-  //     return isSortedAsc ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
-  //   });
-  //   setQuestions([...sortedQuestions]);
-  //   setIsSortedAsc(!isSortedAsc);
-  // };
-
   return (
     <button className='bluebtn' onClick={handleNewQuestion}>
       Create a forum
