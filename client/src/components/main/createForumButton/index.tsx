cf29241c06c782571bd874017fa10820b1daefa0
Alexandra Hu <108170281+allexandrahu@users.noreply.github.com>
2025-03-25 13:04:18 -0400

    finish sort options

--- client/src/components/main/createForumButton/index.tsx
+++ client/src/components/main/createForumButton/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
 /**
  * AskQuestionButton component that renders a button for navigating to the
  * "New Forum" page. When clicked, it redirects the user to the page
@@ -16,12 +14,18 @@ const CreateForumButton = () => {
     navigate('/new/forum');
   };
 
+  // const handleSortQuestions = () => {
+  //   const sortedQuestions = questions.sort((a, b) => {
+  //     const dateA = new Date(a.createdAt);
+  //     const dateB = new Date(b.createdAt);
+  //     return isSortedAsc ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
+  //   });
+  //   setQuestions([...sortedQuestions]);
+  //   setIsSortedAsc(!isSortedAsc);
+  // };
+
   return (
-    <button
-      className='bluebtn'
-      onClick={() => {
-        handleNewQuestion();
-      }}>
+    <button className='bluebtn' onClick={handleNewQuestion}>
       Create a forum
     </button>
   );
