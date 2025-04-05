e8b410ccd4a9b1e581bfc2cee5dfb297280c885b
RaisaBh <raisa16h21@gmail.com>
2025-04-05 11:02:14 -0400

    progres

--- client/src/components/header/notificationButton/index.tsx
+++ client/src/components/header/notificationButton/index.tsx
@@ -7,39 +7,17 @@ import NotificationCard from '../notificationCard';
  * Notification component renders a page for direct messaging between users.
  * It includes a list of users and a chat window to send and receive messages.
  */
-const NotificationButton = () => {
-  const {
-    showBrowserNotifs,
-    setShowBrowserNotifs,
-    unreadBrowserNotifs,
-    error,
-    handleReadNotification,
-  } = useNotification();
-
-  return (
-    <>
-      {error && <div className='notif-error'>{error}</div>}
-      <div className='notif-section'>
-        <button
-          className='dropdown-button'
-          data-toggle='dropdown'
-          onClick={() => setShowBrowserNotifs(prev => !prev)}>
-          Notifications
-        </button>
-        {showBrowserNotifs && (
-          <div>
-            {unreadBrowserNotifs.map(bNotif => (
-              <NotificationCard
-                key={String(bNotif._id)}
-                notification={bNotif}
-                handleReadNotification={() => handleReadNotification(bNotif._id)}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-    </>
-  );
-};
+const NotificationButton = () => (
+  <>
+    <div className='notif-section'>
+      <button
+        className='dropdown-button'
+        data-toggle='dropdown'
+        onClick={() => console.log('cool')}>
+        Notifications
+      </button>
+    </div>
+  </>
+);
 
 export default NotificationButton;
