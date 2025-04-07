25cf31e59fafeed3ec07951609b663f209c15c33
RaisaBh <raisa16h21@gmail.com>
2025-04-07 13:29:24 -0400

    main-updates

--- client/src/components/main/notificationPage/index.tsx
+++ client/src/components/main/notificationPage/index.tsx
@@ -5,48 +5,87 @@ import NotificationCard from './notificationCard';
 
 const NotificationPage = () => {
   const { unreadBrowserNotifs, error, handleReadNotification } = useNotification();
+
+  const ansFilter = unreadBrowserNotifs.filter(notif =>
+    notif.title.includes('New Answer to Your Post'),
+  );
+  const commentFilter = unreadBrowserNotifs.filter(notif =>
+    notif.title.includes('New Comment to Your Post'),
+  );
+  const badgeFilter = unreadBrowserNotifs.filter(notif => notif.title.includes('New Badge Added'));
+  const forumPostFilter = unreadBrowserNotifs.filter(notif =>
+    notif.title.includes('New Post in One of Your Forums'),
+  );
+  const forumPermissionFilter = unreadBrowserNotifs.filter(notif =>
+    notif.title.includes('Forum Permission Update'),
+  );
+
+  const ansTitle =
+    ansFilter.length > 0
+      ? `Answers to Your Question (${ansFilter.length})`
+      : 'Answers to Your Question';
+  const comTitle =
+    commentFilter.length > 0
+      ? `Comments to Your Questions/Answers (${commentFilter.length})`
+      : 'Comments to Your Questions/Answers';
+  const badgeTitle = badgeFilter.length > 0 ? `New Badges (${badgeFilter.length})` : 'New Badges';
+  const forumPostTitle =
+    forumPostFilter.length > 0
+      ? `New Post in Forum (${forumPostFilter.length})`
+      : 'New Post in Forum';
+  const forumPermissionTitle =
+    forumPermissionFilter.length > 0
+      ? `Update to Forum Permissions (${forumPermissionFilter.length})`
+      : 'Update to Forum Permissions';
+
   return (
     <>
       {error && <p>{error}</p>}
       <Tabs defaultActiveKey='answers' id='justify-tab-example' justify>
-        <Tab eventKey='answers' title='Answers to Your Questions'>
-          {unreadBrowserNotifs
-            .filter(notif => notif.title.includes('New Answer to Your Post'))
-            .map(bNotif => (
-              <NotificationCard
-                key={String(bNotif._id)}
-                notification={bNotif}
-                handleReadNotification={() => handleReadNotification(bNotif._id)}
-              />
-            ))}
+        <Tab eventKey='answers' title={ansTitle}>
+          {ansFilter.map(bNotif => (
+            <NotificationCard
+              key={String(bNotif._id)}
+              notification={bNotif}
+              handleReadNotification={() => handleReadNotification(bNotif._id)}
+            />
+          ))}
         </Tab>
-        <Tab eventKey='comments' title='Comments to Your Questions/Answers'>
-          {unreadBrowserNotifs
-            .filter(notif => notif.title.includes('New Comment to Your Post'))
-            .map(bNotif => (
-              <NotificationCard
-                key={String(bNotif._id)}
-                notification={bNotif}
-                handleReadNotification={() => handleReadNotification(bNotif._id)}
-              />
-            ))}
+        <Tab eventKey='comments' title={comTitle}>
+          {commentFilter.map(bNotif => (
+            <NotificationCard
+              key={String(bNotif._id)}
+              notification={bNotif}
+              handleReadNotification={() => handleReadNotification(bNotif._id)}
+            />
+          ))}
         </Tab>
-        <Tab eventKey='badge' title='New Badges'>
-          {unreadBrowserNotifs
-            .filter(notif => notif.title.includes('You have received a new badge'))
-            .map(bNotif => (
-              <NotificationCard
-                key={String(bNotif._id)}
-                notification={bNotif}
-                handleReadNotification={() => handleReadNotification(bNotif._id)}
-              />
-            ))}
+        <Tab eventKey='badge' title={badgeTitle}>
+          {badgeFilter.map(bNotif => (
+            <NotificationCard
+              key={String(bNotif._id)}
+              notification={bNotif}
+              handleReadNotification={() => handleReadNotification(bNotif._id)}
+            />
+          ))}
         </Tab>
-        <Tab eventKey='forum' title='New Posts in Forum'>
-          Tab content for Contact
+        <Tab eventKey='forum' title={forumPostTitle}>
+          {forumPostFilter.map(bNotif => (
+            <NotificationCard
+              key={String(bNotif._id)}
+              notification={bNotif}
+              handleReadNotification={() => handleReadNotification(bNotif._id)}
+            />
+          ))}
         </Tab>
-        <Tab eventKey='permissions' title='Forum Permissions'>
-          Tab content for Contact2
+        <Tab eventKey='permissions' title={forumPermissionTitle}>
+          {forumPermissionFilter.map(bNotif => (
+            <NotificationCard
+              key={String(bNotif._id)}
+              notification={bNotif}
+              handleReadNotification={() => handleReadNotification(bNotif._id)}
+            />
+          ))}
         </Tab>
       </Tabs>
     </>
